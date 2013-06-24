f567ea11fb493136245208cf5b9b1bb41e1f9b5c
Paul van der Meijs <paul@lowtone.nl>
2013-06-24 18:52:18 +0200

    Included Google Picker.

--- lowtone-media-google-drive.php
+++ lowtone-media-google-drive.php
@@ -19,7 +19,8 @@
 namespace lowtone\media\google\drive {
 
 	use lowtone\content\packages\Package,
-		lowtone\media\types\Type;
+		lowtone\media\types\Type,
+		lowtone\google\picker\Picker;
 
 	// Includes
 	
@@ -27,13 +28,40 @@ namespace lowtone\media\google\drive {
 		return trigger_error("Lowtone Content plugin is required", E_USER_ERROR) && false;
 
 	$__i = Package::init(array(
-			Package::INIT_PACKAGES => array("lowtone", "lowtone\\media"),
+			Package::INIT_PACKAGES => array("lowtone", "lowtone\\media", "lowtone\\google\\picker"),
 			Package::INIT_MERGED_PATH => __NAMESPACE__,
 			Package::INIT_SUCCESS => function() {
 
 				// Register textdomain
 				
 				load_plugin_textdomain("lowtone_media_google_drive", false, basename(__DIR__) . "/assets/languages");
+
+				// Create Google Picker
+
+				$picker = new Picker(array(
+						Picker::PROPERTY_ID => "lowtone_media_google_drive",
+						Picker::PROPERTY_VIEWS => array(
+								Picker::VIEW_DOCS_IMAGES_AND_VIDEOS,
+								Picker::VIEW_DOCUMENTS,
+							),
+						Picker::PROPERTY_CALLBACK => function() {
+							header("Content-type: application/json");
+
+							echo json_encode(array(
+									"meta" => array(
+										"code" => 200,
+										"message" => array(
+											"Success!"
+										)
+									),
+									"data" => array(
+										"text" => "You picked {$_GET['url']}",
+									)
+								));
+
+							exit;
+						}
+					));
 
 				// Add media type
 
@@ -42,12 +70,14 @@ namespace lowtone\media\google\drive {
 						Type::PROPERTY_NEW_FILE_TEXT => __("Import a file from Google Drive.", "lowtone_media_google_drive"),
 						Type::PROPERTY_SLUG => "google-drive",
 						Type::PROPERTY_IMAGE => plugins_url("/assets/images/google-drive-icon.png", __FILE__),
-						Type::PROPERTY_NEW_FILE_CALLBACK => function() {
+						Type::PROPERTY_NEW_FILE_CALLBACK => function() use ($picker) {
 
 							echo '<div class="wrap">' . 
 								get_screen_icon() . 
 								'<h2>' . __("Select a file on Google Drive", "lowtone_media_google_drive") . '</h2>' . 
 								'<p>' . __("Select the file you want to import.", "lowtone_media_google_drive") . '</p>';
+
+							echo $picker->button();
 
 							echo '</div>';
 							
